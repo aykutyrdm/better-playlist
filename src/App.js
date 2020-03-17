42fa4d61a09208e4424fdc5313931afe75f818e7
aykutyrdm <aykutyrdm@gmail.com>
2020-03-17 23:00:47 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -7,14 +7,71 @@ import './App.css';
 let aggregateTextColor = 'green'
 let filterTextColor = 'red'
 let PlayListTextColor = 'black'
-let headerSize = '50px'
+//let headerSize = '50px'
 //let headerStyle = {color : headerColor, 'font-size' : headerSize}
 
-class Aggregate extends Component {
+
+let fakeServerData = {
+  user : {
+    name : 'Aykut',
+    playLists : [
+      {
+        name : 'My favorities',
+        songs: [
+          {name : 'Hotel California', duration : 7300}, 
+          {name : 'People are strange', duration : 2400}, 
+          {name : 'November rain', duration : 7800}
+        ]
+      },
+      {
+        name : 'This weeks',
+        songs : [
+          {name : 'Day dream', duration : 7300}, 
+          {name : 'Listen to rain', duration : 7300 }, 
+          {name : 'Daddy cool', duration : 7300}
+        ]
+      },
+      {
+        name : 'Best deep house',
+        songs : [
+          {name : 'Feel', duration : 7300}, 
+          {name : 'Liranov', duration : 7300}, 
+          {name : 'Hoizer', duration : 7300}
+        ]
+      },
+      {
+        name : 'Turkish',
+        songs : [
+          {name : 'Eylulde gel', duration : 7300} ,
+          {name : 'Ayrılanlar icin', duration : 7300},
+          {name : 'Ispanyol meyhanesi', duration : 7300}
+        ]
+      }
+    ]  
+  }
+}
+
+class PlayListCounter extends Component {
   render(){
     return(
       <div style = {{width : '40%' , display : 'inline-block'}}>
-        <h2 style = { {color : aggregateTextColor} }> Number Text</h2>
+        <h2 style = { {color : aggregateTextColor} }> {this.props.playLists && this.props.playLists.length} PlayLists</h2>
+      </div>
+    )
+  }
+}
+
+class HoursCounter extends Component {
+  render(){
+    let allSongs = this.props.playLists.reduce((songs,eachPlayList) => {
+      return songs.concat(eachPlayList.songs)
+    }, []);
+    let totalDuration = allSongs.reduce((sum,eachSong) => {
+      return sum + eachSong.duration
+    },0)
+    return(
+      <div style = {{width : '40%' , display : 'inline-block'}}>
+        <h2 style = { {color : aggregateTextColor} }> {(totalDuration / (60*1000)).toFixed(1)} Hours</h2>
       </div>
     )
   }
@@ -34,6 +91,8 @@ class Filter extends Component {
 }
 
 class Playlist extends Component {
+
+  
   render(){
     return(
       <div style = { {color : PlayListTextColor, width : '25%', display : 'inline-block'} }>
@@ -47,19 +106,45 @@ class Playlist extends Component {
     );
   }
 }
+
+
+class Render extends Component{
+
+  constructor() {
+    super();
+    this.state = {serverData : {}};
+  }
+
+  componentDidMount(){
+    setTimeout( () => {
+      this.setState({serverData : fakeServerData});
+   } ,2000)
+  }
+
+  render(){
+    return(
+      <div className="App">
+        {this.state.serverData.user ?
+        <div>
+          <h1> {this.state.serverData.user.name}'s PlayList</h1>
+          <PlayListCounter playLists = {this.state.serverData.user && this.state.serverData.user.playLists}/>
+          <HoursCounter playLists = {this.state.serverData.user && this.state.serverData.user.playLists}/>
+          <Filter/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+        </div> : <h1> ...Loading </h1>
+        }
+    </div>
+    )
+  }
+}
   
 function App() {
+  
   return (
-    <div className="App">
-      <h1> Title</h1>
-      <Aggregate/>
-      <Aggregate/>
-      <Filter/>
-      <Playlist/>
-      <Playlist/>
-      <Playlist/>
-      <Playlist/>
-    </div>
+    <Render/>
   );
 }
 
