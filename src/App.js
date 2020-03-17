98ded1fe20c7dd446d46221cdb11780d9af63abb
aykutyrdm <aykutyrdm@gmail.com>
2020-03-17 20:23:06 +0300

    playlist_v0.1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
+
+//let headerName = 'OKUL WEB IS GROWING'
+let aggregateTextColor = 'green'
+let filterTextColor = 'red'
+let PlayListTextColor = 'black'
+//let headerSize = '50px'
+//let headerStyle = {color : headerColor, 'font-size' : headerSize}
+
+class Aggregate extends Component {
+  render(){
+    return(
+      <div style = {{width : '40%' , display : 'inline-block'}}>
+        <h2 style = { {color : aggregateTextColor} }> Number Text</h2>
+      </div>
+    )
+  }
+}
+
+class Filter extends Component {
+
+  render(){
+    return(
+      <div style = { {color : filterTextColor} }>
+      <img/>
+      <input type = 'text'/>
+      Filter 
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render(){
+    return(
+      <div style = { {color : PlayListTextColor, width : '25%', display : 'inline-block'} }>
+        <img/>
+        <h3> PlayList</h3>
+        <ul>
+        <li>Song 1</li>
+        <li>Song 2</li>
+        </ul>
+      </div>
+    );
+  }
+}
+  
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Title</h1>
+      <Aggregate/>
+      <Aggregate/>
+      <Filter/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
     </div>
   );
 }
